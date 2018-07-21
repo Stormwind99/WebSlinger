64d42c1b4d909f78d3b542af1a5de8a717a9379b
Jason Spangler <jasons@wumple.com>
2018-07-20 20:19:35 -0500

    Jar signing, updated Forge and mappings, and build.gradle automation

---
+++ src/main/java/com/wumple/webslinger/Reference.java
@@ -0,0 +1,27 @@
+package com.wumple.webslinger;
+
+public class Reference
+{
+    // Mod Constants
+
+    // /*
+    public static final String MOD_ID = "@MOD_ID@";
+    public static final String MOD_NAME = "@MOD_NAME@";
+    public static final String BUILD = "@BUILD@";
+    public static final String MOD_VERSION = "@VERSION@";
+    public static final String DEPENDENCIES = "@DEPENDENCIES@";
+    public static final String UPDATEJSON = "@UPDATEJSON@";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
+    // */
+
+    /*
+    // debugger settings since no Gradle string replacement
+    public static final String MOD_ID = "webslinger";
+    public static final String MOD_NAME = "WebSlinger";
+    public static final String BUILD = "99";
+    public static final String MOD_VERSION = "9.9.9";
+    public static final String DEPENDENCIES = "";
+    public static final String UPDATEJSON = "update.json";
+    public static final String FINGERPRINT = "";
+    */
+}
--- src/main/java/com/wumple/webslinger/WebSlinger.java
+++ src/main/java/com/wumple/webslinger/WebSlinger.java
@@ -1,10 +1,12 @@
 package com.wumple.webslinger;
 
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import josephcsible.webshooter.PlayerInWebMessage;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -12,7 +14,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, updateJSON=Reference.UPDATEJSON)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, dependencies = Reference.DEPENDENCIES, updateJSON = Reference.UPDATEJSON, certificateFingerprint=Reference.FINGERPRINT)
 public class WebSlinger
 {
 	@Mod.Instance(Reference.MOD_ID)
@@ -37,4 +39,18 @@ public class WebSlinger
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{ }
+
+    @EventHandler
+    public void onFingerprintViolation(FMLFingerprintViolationEvent event)
+    {
+        if (logger == null)
+        {
+            logger = LogManager.getLogger(Reference.MOD_ID);
+        }
+        if (logger != null)
+        {
+            logger.warn("Invalid fingerprint detected! The file " + event.getSource().getName() + " may have been tampered with. This version will NOT be supported by the author!");
+            logger.warn("Expected " + event.getExpectedFingerprint() + " found " + event.getFingerprints().toString());
+        }
+    }
 }
